99c20c64cfa5088fdbf66bfbc38e92ee05e49de1
Whittier Heard <wheard513@gmail.com>
2017-12-22 18:02:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
+
+function happyHolisdayTo(holiday, name) {
+  return `happy ${holiday}, ${name}!`
+}
